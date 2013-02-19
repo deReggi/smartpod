a6fa2b01d4a7787efce3c1e6b2f2dba108d001f0
deReggi <Andreas@Reggian.local>
2013-02-19 14:19:06 +0100

    Some communication code commited.

--- SmartPod/src/smartpod/Communicator.java
+++ SmartPod/src/smartpod/Communicator.java
@@ -1,5 +1,7 @@
 package smartpod;
 
+import jade.lang.acl.ACLMessage;
+
 /**
  *
  * @author Andreas
@@ -7,4 +9,11 @@ package smartpod;
 public class Communicator
 {
 	
+	public ACLMessage createMessageQueryIf(String ontology, String content)
+    {
+		ACLMessage msg = new ACLMessage(ACLMessage.QUERY_IF);
+		msg.setOntology(ontology);
+		msg.setContent(content);
+		return msg;
+    }
 }
